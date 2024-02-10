a362d40a12b55d9969094a981c667c8316d8f738
baglaaarav <aaravispossible19@gmail.com>
2024-02-10 10:36:36 +0530

    changes

---
+++ src/components/News.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export default class News extends Component {
+  render() {
+    return (
+      <div className='container my-3'>
+        <h2>NewsMonkey-Top headline</h2>
+
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title={"mytitle"} description={"this is a description"} />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title={"mytitle"} description={"this is a description"} />
+          </div><div className="col-md-4">
+            <NewsItem title={"mytitle"} description={"this is a description"} />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title={"mytitle"} description={"this is a description"} />
+          </div><div className="col-md-4">
+            <NewsItem title={"mytitle"} description={"this is a description"} />
+          </div><div className="col-md-4">
+            <NewsItem title={"mytitle"} description={"this is a description"} />
+          </div>
+        </div>
+
+        This is news component
+      </div>
+    )
+  }
+}
