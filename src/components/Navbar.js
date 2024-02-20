93cf5d86d04be175efba7f9e272a0a59253027fe
baglaaarav <aaravispossible19@gmail.com>
2024-02-20 18:33:39 +0530

    m

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,18 +5,18 @@ export class Navbar extends Component {
     // static propTypes = {};
 
     render() {
-        
+
         return (
             <div>
                 <nav className={`navbar navbar-expand-lg  navbar-${this.props.mode} bg-${this.props.mode}`}>
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="/">
-                            NewzMonkey
-                        </a>
-                        
-                        <div className="collapse navbar-collapse">
+                        <a className="navbar-brand" href="/">  NewzMonkey </a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                
+
                                 <li className="nav-item"><a className="nav-link" href="/about">Business</a></li>
                                 <li className="nav-item"><a className="nav-link" href="/about">Entertainment</a></li>
                                 <li className="nav-item"><a className="nav-link" href="/about">General</a></li>
@@ -26,13 +26,13 @@ export class Navbar extends Component {
                                 <li className="nav-item"><a className="nav-link" href="/about">Technology</a></li>
 
                             </ul>
-
+                            <div className={`form-check form-switch text-${this.props.mode === 'light' ? 'dark' : 'light'} `}>
+                                <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={this.props.toggle} />
+                                <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Dark Mode</label>
+                            </div>
                         </div>
                     </div>
-                    <div className={`form-check form-switch text-${this.props.mode === 'light'?'dark':'light'} ` }>
-                        <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={this.props.toggle} />
-                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Dark Mode</label>
-                    </div>
+
                 </nav>
 
 
